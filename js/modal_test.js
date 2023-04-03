6f1234b072567fddb5e88920abbae68eef85c10a
vora137 <vora137@naver.com>
2023-04-04 00:31:57 +0900

    캘린더 모달 수정중

--- js/modal_test.js
+++
@@ -1,9 +0,0 @@
-function show() {
-  document.querySelector('.background').className = 'background show';
-}
-function close() {
-  document.querySelector('.background').className = 'background';
-}
-
-document.querySelector('#show').addEventListener('click', show);
-document.querySelector('#close').addEventListener('click', close);
--- js/mypage_pet_modal.js
+++ js/mypage_pet_modal.js
@@ -1,65 +1,19 @@
-/* 사진업로드 */
-function DropFile(dropAreaId, fileListId) {
-  let dropArea = document.getElementById(dropAreaId);
-  let fileList = document.getElementById(fileListId);
-
-  function preventDefaults(e) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
-
-  function highlight(e) {
-    preventDefaults(e);
-    dropArea.classList.add('highlight');
-  }
-
-  function unhighlight(e) {
-    preventDefaults(e);
-    dropArea.classList.remove('highlight');
-  }
-
-  function handleDrop(e) {
-    unhighlight(e);
-    let dt = e.dataTransfer;
-    let files = dt.files;
-
-    handleFiles(files);
-
-    const fileList = document.getElementById(fileListId);
-    if (fileList) {
-      fileList.scrollTo({ top: fileList.scrollHeight });
-    }
-  }
-
-  function handleFiles(files) {
-    files = [...files];
-    // files.forEach(uploadFile);
-    files.forEach(previewFile);
-  }
-
-  function previewFile(file) {
-    console.log(file);
-    renderFile(file);
-  }
-
-  function renderFile(file) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function () {
-      let img = dropArea.getElementsByClassName('preview')[0];
-      img.src = reader.result;
-      img.style.display = 'block';
-    };
-  }
-
-  dropArea.addEventListener('dragenter', highlight, false);
-  dropArea.addEventListener('dragover', highlight, false);
-  dropArea.addEventListener('dragleave', unhighlight, false);
-  dropArea.addEventListener('drop', handleDrop, false);
-
-  return {
-    handleFiles,
-  };
+// 모달 열기 버튼 클릭 시 실행되는 함수
+function openModal() {
+  var modal = document.querySelector('.pet_modal');
+  modal.style.display = 'block';
 }
 
-const dropFile = new DropFile('drop-file', 'files');
+// 모달 닫기 버튼 클릭 시 실행되는 함수
+function closeModal() {
+  var modal = document.querySelector('.pet_modal');
+  modal.style.display = 'none';
+}
+
+// 모달 외부 클릭 시 모달 닫기
+window.onclick = function (event) {
+  var modal = document.querySelector('.pet_modal');
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
